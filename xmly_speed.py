47be49bd6c060ba7a321c56d09d176f3c47d00d8
luyiwind <70989874+luyiwind@users.noreply.github.com>
2020-10-26 15:53:20 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -543,8 +543,8 @@ def account(cookies,index):
 
 """)
 
-    #if (result["total"]/10000 > 1.0):
-    response = requests.get(
+    if (result["total"]/10000 > 19.0):
+        response = requests.get(
             f"""https://sc.ftqq.com/SCU120556Teb7de72e0c3dec7a61d47017d080e19f5f967dd7750b5.send?text=账号{index}&desp=剩余{result["total"]/10000} >= 20元，可以提现！！！""")
 
 
